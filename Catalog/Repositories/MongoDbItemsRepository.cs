b964904dd0adae9bf2aa2776c2b3002ca3a7d8a6
Kyle Bautista <kylebautista@Kyles-M1-MacBook-Pro.local>
2021-11-30 20:10:55 -0700

    setup init MongoDb service

---
+++ Catalog/Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+
+namespace Catalog.Repositories{
+  public class MongoDbItemsRepository : IItemsRespository
+  {
+    public MongoDbItemsRepository()
+    {
+    }
+
+    public void CreateItem(Item item)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void DeleteItem(Guid id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Item GetItem(Guid id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public IEnumerable<Item> GetItems()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void UpdateItem(Item item)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
