329f037334e1a62ca83b3107463cbd024fb365d5
Kyle Bautista <kylebautista@Kyles-M1-MacBook-Pro.local>
2021-11-30 19:34:03 -0700

    add UpdateItem service

--- Catalog/Repositories/IItemsRepository.cs
+++ Catalog/Repositories/IItemsRepository.cs
@@ -7,7 +7,7 @@ namespace Catalog.Repositories{
     public interface IItemsRespository{
         Item GetItem(Guid id);
         IEnumerable<Item> GetItems();
-
         void CreateItem(Item item);
+        void UpdateItem(Item item);
     }
 }
